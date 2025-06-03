e146ee78c4ad55eae79d4eeb310ea754eda50892
Yana Ivanova <yanaivashka@yandex.ru>
2025-06-03 19:13:48 +0300

    save

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/task-list/",
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: "./setupTests.ts",
-    base: "/task-list/",
   },
 });
