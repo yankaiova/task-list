0adb5dc4f0557739fcf10dfd42a480584de4dcad
Yana Ivanova <yanaivashka@yandex.ru>
2025-06-03 18:22:06 +0300

    app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import { ButtonAdd } from "./components/buttonAdd/ButtonAdd";
+import type { Task } from "./types";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { TabList } from "./components/tabs/TabList";
+import { TaskComponent } from "./components/task/Task";
+import { useTasks } from "./hooks/useTasks";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [activeTab, setActiveTab] = useState<string>("all");
+
+  const {
+    clearCompleted,
+    addTask,
+    toggleTask,
+    totalCompletedTask,
+    getActiveList,
+  } = useTasks();
+
+  const filteredList = getActiveList(activeTab);
+
+  return (
+    <div>
+      <ButtonAdd addTask={addTask} />
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <TabList value={activeTab} setActiveTab={setActiveTab} />
+        <div>
+          {filteredList.map((task: Task) => (
+            <TaskComponent
+              key={task.value}
+              value={task.value}
+              status={task.status}
+              toggleTask={toggleTask}
+            />
+          ))}
+        </div>
+      </Box>
+      <Stack
+        mt={"20px"}
+        direction={"row"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Typography color="info">completed {totalCompletedTask}</Typography>
+        <Button variant="outlined" onClick={clearCompleted}>
+          Clear completed
+        </Button>
+      </Stack>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export type Task = {
+  value: string;
+  status: boolean;
+};
